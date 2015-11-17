5305fbfd7636bd15d247c94888ced56a1362ef4f
rahul bhawsar <bhawsar.rahul12@yahoo.com>
2015-11-17 23:12:46 +0530

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var app = angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+
+app.controller("myCtrl", function($scope) {
+    $scope.message = "";
+    $scope.left  = function() {return 100 - $scope.message.length;};
+    $scope.clear = function() {$scope.message = "";  
+    $scope.name= "";
+    $scope.age= "";
+    $scope.employer= "";
+    $scope.ctc= "";};
+    $scope.save  = function() {alert("Data Saved");};
+});
