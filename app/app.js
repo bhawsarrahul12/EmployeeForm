936ae1e80e2d289b7265e4797c651aedd0962c74
rahul bhawsar <bhawsar.rahul12@yahoo.com>
2015-11-18 11:07:11 +0530

    Updated app.js file

--- app/app.js
+++ app/app.js
@@ -1,15 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var app = angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+var app = angular.module("myApp", []);
 
 app.controller("myCtrl", function($scope) {
     $scope.message = "";
